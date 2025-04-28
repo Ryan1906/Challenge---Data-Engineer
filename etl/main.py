9880a120019a8accd147cd4c3571c15366921d63
Ryan1906 <rjrshuertas@gmail.com>
2025-04-28 11:02:33 -0600

    UPdating pivot tables

--- etl/main.py
+++ etl/main.py
@@ -8,16 +8,29 @@ from etl.create_normalized import create_normalized_table
 from etl.s3_utils import download_json_from_s3
 from etl.pivot_data import pivot_data
 
+
 # Cargar variables de entorno desde el archivo .env
 load_dotenv()
+
+# Definir BASE_DIR y ajustar la ruta para apuntar a la carpeta 'data' fuera de 'etl'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Subir un nivel
+DOWNLOAD_PATH = os.path.join(BASE_DIR, "data", "laliga_2009_2010_matches.json")
 
 if __name__ == "__main__":
     # Configuración de S3 desde el archivo .env
     BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
-    OBJECT_KEY = os.getenv("S3_OBJECT_KEY")
-    DOWNLOAD_PATH = os.getenv("DOWNLOAD_PATH")
+    OBJECT_KEY = os.getenv("OBJECT_KEY")
     AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    
+    print(f"BUCKET_NAME: {BUCKET_NAME}")
+    print(f"OBJECT_KEY: {OBJECT_KEY}")
+    print(f"DOWNLOAD_PATH: {DOWNLOAD_PATH}")
+
+    # Crear la carpeta 'data' si no existe
+    download_dir = os.path.dirname(DOWNLOAD_PATH)
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
 
     # Descargar el archivo JSON desde S3
     download_json_from_s3(BUCKET_NAME, OBJECT_KEY, DOWNLOAD_PATH, AWS_ACCESS_KEY, AWS_SECRET_KEY)
